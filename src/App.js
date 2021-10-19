7266972d74e6bfd2357ca43020693dfed4433c34
sandraoc <sandrus.smo@gmail.com>
2021-10-19 08:27:00 +0200

    files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import Questions from './components/data/Questions.json';
+import "./styles.css";
 
 function App() {
+
+  const [question, setQuestion] = useState(0);
+  const [end, setEnd] = useState(false);
+  const [help, nextHelp] = useState(0);
+  
+
+  const handleAnswer = (correct) => {
+    const nextQuestion = question + 1;
+    const nextHelper = help + 1;
+    const helpStatement = Questions.questions[question].helper;
+
+    if (nextQuestion < Questions.questions.length && correct === true){
+      setQuestion(nextQuestion);
+      nextHelp(nextHelper);
+    } else if (correct === false){
+      console.log(helpStatement);
+    } else {
+      setEnd(true);
+    } 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>TESTING QUIZ</h1>
+    
+      {end ? (<p>Gratulerer! Du er ferdig med Quizen.</p>)
+      : (
+        <section id="qna">
+        <h2>{Questions.questions[question].title}</h2>
+        <section id="btn-wrapper">
+        {Questions.questions[question].answers.map((answers, i) => (<button type="button" key={i} onClick={()=>handleAnswer(answers.correct)}>{answers.answere}</button>))}
+        </section>
+        <p></p>
+      </section>
+      )}
+    </>
   );
 }
 
